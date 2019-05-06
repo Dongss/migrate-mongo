8447075fa9baa07192594f89b52b9fdbe6521daf
Dongss <dongsoso@hotmail.com>
2019-05-06 10:48:18 +0800

    release v0.1.0 close #15

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## v0.1.0 2019-05-06
+
+Features:
+
+* Migrate mongo all data from db to db
+* Migrate mongo data from db to db by specified collections
+* Migrate mongo data include indexes, create indexes before inserting data
+* Batch inserting for migration
+* Interval between each single intersing
--- cmd/verison.go
+++ cmd/verison.go
@@ -6,10 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const version = "v0.1.0"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version of migrate-mongo",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("migrate-mongo v0.1.0")
+		fmt.Println("migrate-mongo", version)
 	},
 }
